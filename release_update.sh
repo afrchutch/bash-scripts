4bc480d358a2f8402ff1406ee5b24e0456c360f5
afrchutch <afrchutch@gmail.com>
2024-06-18 22:06:20 -0600

    added function for checking exit status

--- release_update.sh
+++ release_update.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 release_file=/etc/os-release
+error_log=/var/log/update_error.log
+success_log=/var/log/update_success.log
+
+check_exit_status() {
+    if [ $? -ne 0 ]
+    then
+        echo "An error has occurred, please check the $error_log"
+    else
+        echo "Update successful. Feel free to check the log here: $success_log"
+    fi
+}
 
 if grep -q "Debian" $release_file || grep -q "Ubuntu" $release_file
 then
     # The host is based on Debian/Ubuntu. Run the apt update command.
-    sudo apt update -y
-    sudo apt dist-upgrade -y
+    sudo apt update -y 1>>$success_log 2>>$error_log 
+    sudo apt dist-upgrade -y 1>>$success_log 2>>$error_log
+    check_exit_status
+
 fi
